b583cd96ce419987b92b3e8a0c5655e04cfc7f0e
mikeleo03 <leonmichael463@gmail.com>
2025-04-23 23:33:54 +0700

    [Feat] Support session router

--- app/api/main.py
+++ app/api/main.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.routes import upload, ws
+from app.api.routes import upload, ws, sessions
 
 api_router = APIRouter()
+api_router.include_router(sessions.router)
 api_router.include_router(upload.router)
 api_router.include_router(ws.router)
